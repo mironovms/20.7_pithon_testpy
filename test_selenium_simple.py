7efb52670a3cb3ae907569b0bc922abd863a7032
Easy Rider <115343312+mironovms@users.noreply.github.com>
2023-02-12 23:24:17 +0300

    welldone!

--- test_selenium_simple.py
+++ test_selenium_simple.py
@@ -1,19 +1,3 @@
-#!/usr/bin/python3
-# -*- encoding=utf8 -*-
-
-# You can find very simple example of the usage Selenium with PyTest in this file.
-#
-# More info about pytest-selenium:
-#    https://pytest-selenium.readthedocs.io/en/latest/user_guide.html
-#
-# How to run:
-#  1) Download geko driver for Chrome here:
-#     https://chromedriver.storage.googleapis.com/index.html?path=2.43/
-#  2) Install all requirements:
-#     pip install -r requirements.txt
-#  3) Run tests:
-#     python3 -m pytest -v --driver Chrome --driver-path /tests/chrome test_selenium_simple.py
-#
 
 import time
 from selenium import webdriver
